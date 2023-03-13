99858851b3a5d5415c65f038f083ec71bdc69f06
Onuh Uche <onuhblaze@gmail.com>
2023-03-13 18:37:09 +0100

    channel detail

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -18,14 +18,39 @@ const VideoCard = ({
   },
 }) => {
   return (
-    <Card>
+    <Card
+      sx={{
+        width: { md: "310px", xs: "100%" },
+        boxShadow: "none",
+        borderRadius: 0,
+      }}
+    >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
-          image={snippet?.thumbnails?.high?.url}
-          alt={snippet?.title}
-          sx={{width: 350, height: 180}}
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
+            alt={snippet?.title}
+          sx={{ width: '310px', height: 180 }}
         />
       </Link>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography varient="subtitle1" fontWeight="bold" color="#fff">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography varient="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
